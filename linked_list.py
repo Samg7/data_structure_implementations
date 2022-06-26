a43e2c4d9bf6f7eb8601d9e8e7b7346b50bf79ed
Samg7 <14854937+Samg7@users.noreply.github.com>
2022-06-26 17:03:47 -0500

    First commit

---
+++ linked_list.py
@@ -0,0 +1,44 @@
+'''
+Linked List implementation
+Date: June 25, 2022 (06.25.2022)
+'''
+
+class Node(object):
+    def __init__(self, data=None, next=None):  # Node constructor, attributes default to None if no arguements are passed
+        self.data = data
+        self.next = next
+        
+class LinkedList:
+    def __init__(self):  # LinkedList constructor
+        self.head = None  # Default constructor creates a head with no reference
+        
+    # Append to the end of the linked list
+    def append(self, new_element):
+        new_node = Node(new_element, None)
+        # Approach: determine whether to append after head or set new_element as the head
+        if self.head is not None:
+            current = self.head
+            while current.next is not None:  # I can shorten by using "while current.next:" 
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def print(self):
+        if self.head is not None:
+            current = self.head
+            list_contents = '[' + str(current.data)
+            while current:
+                list_contents += str(current.data)  # Implement fence post algorithm for '4, 5, 6' look
+                current = current.next
+            list_contents += ']'
+            print(list_contents)
+        else:
+            print("Linked List is empty")
+
+ll = LinkedList()
+ll.print()
+ll.append(4)
+ll.append(5)
+ll.append(6)
+ll.print()
