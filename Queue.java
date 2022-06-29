e14c509fbd76e77a4fca4c02c89eb9511c51de16
Samg7 <14854937+Samg7@users.noreply.github.com>
2022-06-29 16:15:23 -0500

    Add additional queue functions

--- Queue.java
+++ Queue.java
@@ -22,21 +22,32 @@ public class Queue {
      * place integer e on the rear of the queue 
      * */
     public void enqueue(int e) {
-
+        // If queue is half full create a new queue with double the space
+        if (rear >= capacity / 2) {
+            copy();
+            System.out.println("Queue was extended...");
+        }
         elements[rear++] = e;
     }
-
+    
     /**
      * remove and return the object at the front of the queue
      * */
-    public void dequeue() {
+    public int dequeue() {
+        if (isEmpty()) {
+            return -1;
+        }
 
+        int dequeuedElement = elements[0];
+        shiftQueue();
+
+        return dequeuedElement;
     }
 
     /**
      * return the integer at the front of the queue without removing it 
      * */
-    public int front() {
+    public int getFront() {
         if (isEmpty()) {
             return -1;
         }
@@ -46,7 +57,7 @@ public class Queue {
     /**
      * return the size of the queue
      * */
-    public int size() {
+    public int getSize() {
         return rear;
     }
 
@@ -61,10 +72,29 @@ public class Queue {
     }
 
     /**
-     * 
+     * return the capacity of the queue
+     */
+    public int getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * Probably not the most efficient way to do things, but I wanted to
+     * test out StringBuilder
      * */
     public String toString() {
-        return "IT WORKS!";
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + elements[front]);
+        for (int i = 1; i < rear; i++) {
+            sb.append(", " + elements[i]);
+        }
+        sb.append("]");
+
+        return sb.toString();
     }
 
     /**
@@ -73,37 +103,49 @@ public class Queue {
     private void copy() {
         int[] temp = new int[capacity * 2];
 
-        for (int i = 0; i < capacity; i++) {
+        for (int i = 0; i < rear; i++) {
             temp[i] = elements[i];
         }
 
-        elements = temp;
-        capacity *= 2;
+        elements = temp;  // overwrite previous array with new dynamically allocated memory
+        capacity *= 2;    // update our capacity to prevent a potential stale variable
     }
 
     /**
      * Shift all elements in the queue to the left by one 
-     * recall, 'rear' acts as '.length' so its value is not zero
-     * indexed
+     * recall, 'rear' acts as a '.length' of sorts
      */
     private void shiftQueue() {
         for (int i = 0; i < rear - 1; i++) {
             elements[i] = elements[i + 1];
         }
+        --rear; // rear is shifted to the left by one
     }
 
     public static void main(String[] args) {
         Queue q1 = new Queue();
-        System.out.println("Is queue empty: " + q1.isEmpty());
-        System.out.println("Size of queue: " + q1.size());
-        System.out.println("Front of the queue: " + q1.front());
         q1.enqueue(1);
-        System.out.println("Is queue empty: " + q1.isEmpty());
-        System.out.println("Size of queue: " + q1.size());
         q1.enqueue(2);
-        System.out.println("Is queue empty: " + q1.isEmpty());
-        System.out.println("Size of queue: " + q1.size());
-        System.out.println("Front of the queue: " + q1.front());
+        q1.enqueue(3);
+        q1.enqueue(4);
+        q1.enqueue(5);
+        System.out.println(q1.toString());
+        System.out.println("Current front element: " + q1.getFront());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println(q1.toString());
+        System.out.println("Current front element: " + q1.getFront());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println(q1.toString());
+        System.out.println("Dequeued element: " + q1.dequeue());
+        System.out.println(q1.toString());
+        q1.enqueue(1);
+        q1.enqueue(2);
+        System.out.println(q1.toString());
+        System.out.println("Current front element: " + q1.getFront());
     }
 }
 
